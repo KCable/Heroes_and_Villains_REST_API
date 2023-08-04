b61b7065ef85fc4bc6961d0802d4f3b843039f43
KCable <126310715+KCable@users.noreply.github.com>
2023-08-03 18:19:33 -0700

    Made changes to Supers Serializers.py file to Improve Functionaliy

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,7 +4,6 @@ from .models import Super
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        heading = 'super_type_id'
         fields =['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
         depth = 1
         
